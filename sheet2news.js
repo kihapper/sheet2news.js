3b56e9399e7df8d695d446b09606243b12391b54
kihapper <playful.intervention@gmail.com>
2020-04-18 15:06:56 +0100

    Applied ES6 syntax to some of the code

--- sheet2news.js
+++ sheet2news.js
@@ -32,7 +32,7 @@ const sheetUrl = "https://spreadsheets.google.com/feeds/list/12f1k677L0LEDuZNvVR
 let googleSheet = [];
 let dateArray = []; 
 let titleArray = []; 
-let newsArray = []; 
+let mainArray = []; 
 
 //Number of latest news you want to show. 
 let newsNumber = 5;
@@ -40,24 +40,24 @@ let newsNumber = 5;
 
 //Fetching Google Sheet
 fetch(sheetUrl)
-    .then(function(response){
+    .then((response) => {
             return response.json();
     })
-    .then(function(data){
+    .then((data) => {
         googleSheet = data.feed.entry;
         console.log(googleSheet);
         
-        for (var i = 0; i < googleSheet.length; i += 1){
+        for (let i = 0; i < googleSheet.length; i += 1){
             dateArray.push(googleSheet[i].gsx$date.$t);
             titleArray.push(googleSheet[i].gsx$title.$t);
-            newsArray.push(googleSheet[i].gsx$description.$t);
+            mainArray.push(googleSheet[i].gsx$description.$t);
         }  
         console.log("Loaded Google Sheets -> Length of Sheet :" + googleSheet.length)
         
         //Trigger initialize content
         updateHtml();
 
-    }).catch(function(){
+    }).catch(()=>{
         //If loading is not successful....
         console.log("Oh no, Can't load Google Docs!");
     })
@@ -71,27 +71,27 @@ updateHtml= () =>{
 
     ——————*/
 
-    for(var i = googleSheet.length - 1 ; i>=googleSheet.length - newsNumber; i--){
+    for(let i = googleSheet.length - 1 ; i>=googleSheet.length - newsNumber; i--){
 
         
         //Create element <p> with the custom class to make it pretty in CSS.   
-        var dateHTML = document.createElement("p");
+        let dateHTML = document.createElement("p");
         dateHTML.className = "gSheet_date";
 
-        var titleHTML = document.createElement("p");
+        let titleHTML = document.createElement("p");
         titleHTML.className = "gSheet_title";
         
-        var newsHTML = document.createElement("p");
+        let newsHTML = document.createElement("p");
         newsHTML.className = "gSheet_news";
       
 
         //Gets the data from JSON and .createTextNode puts it into the dateHTML <p> element
         dateHTML.appendChild(document.createTextNode(dateArray[i]));
-        newsHTML.appendChild(document.createTextNode(newsArray[i]));
+        newsHTML.appendChild(document.createTextNode(mainArray[i]));
         titleHTML.appendChild(document.createTextNode(titleArray[i]));
 
         //Gets div element sheet Update and add the texts
-        var element = document.getElementById("sheetUpdate");
+        let element = document.getElementById("sheetUpdate");
         
         //The order of this effects how the news is lined up.
         element.appendChild(titleHTML);
